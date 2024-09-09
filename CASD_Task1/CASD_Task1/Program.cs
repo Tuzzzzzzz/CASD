cd2e66357228667234609f8359b75242a49de26d
evgen <tuzee101005@gmail.com>
2024-09-09 18:59:20 +0300

    first commit for task1

---
+++ CASD_Task1/CASD_Task1/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    static class VectorCalculator
+    {
+        private static bool IsSymmetrycal(double[][] matrix)
+        {
+            int length = matrix.Length;
+
+            for (int i = 1; i < length; i++)
+                for (int j = 0; j < i; j++)
+                    if (matrix[i][j] != matrix[j][i]) return false;
+
+            return true;
+        }
+
+        public static double CalculateVectorLength(double[][] matrix, double[] vector)
+        {
+            int length = matrix.Length;
+
+            if (!IsSymmetrycal(matrix)) throw new Exception("Not symmetrical");
+
+            double[] newVector = new double[length];
+
+            for (int i = 0; i < length; i++)
+                for (int j = 0; j < length; j++)
+                    newVector[i] += vector[j] * matrix[i][j];
+
+            double resultOfMultiplication = 0;
+
+            for (int i = 0; i < length; i++)
+                resultOfMultiplication += newVector[i] * vector[i];
+
+            return Math.Sqrt(resultOfMultiplication);
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string fileName = "file.txt";
+
+            try
+            {
+                var streamReader = new StreamReader(fileName);
+                int sizeOfMatrix = Convert.ToInt32(streamReader.ReadLine());
+
+                double[][] matrix = new double[sizeOfMatrix][];
+                double[] vector = new double[sizeOfMatrix];
+
+                for (int i = 0; i < sizeOfMatrix; i++)
+                    matrix[i] = streamReader.ReadLine().Split().Select(x => Convert.ToDouble(x)).ToArray();
+
+                vector = streamReader.ReadLine().Split().Select(x => Convert.ToDouble(x)).ToArray();
+
+                Console.WriteLine($"Результат выполнения: {VectorCalculator.CalculateVectorLength(matrix, vector)}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
---
+++ CASD_Task1/CASD_Task1/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("CASD_Task1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CASD_Task1")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("33ab49ee-d526-4709-a48d-8b018bae012a")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
