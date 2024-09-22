63a0e1faa3d7aa0dfcb96773fe53bb9bd07a5208
evgen <tuzee101005@gmail.com>
2024-09-23 00:46:29 +0300

    first commit for task5

---
+++ CASD_Task5/CASD_Task5/MyArrayList.cs
@@ -0,0 +1,176 @@
+﻿namespace MyArrayList;
+internal class MyArrayList<T>
+{
+    private T[] storage;
+    private int size;
+
+    public MyArrayList() : this(10) { }
+
+    public MyArrayList(T[] array) : this()
+    {
+        foreach (T element in array) Add(element);
+    }
+
+    public MyArrayList(int capacity)
+    {
+        if (capacity < 0) throw new ArgumentException();
+        storage = new T[capacity];
+        size = 0;
+    }
+
+    public int Size() => size;
+
+    public bool isEmpty() => size == 0;
+
+    public T this[int index]
+    {
+        get
+        {
+            if (index < 0 || index >= size) throw new IndexOutOfRangeException();
+            return storage[index];
+        }
+        set
+        {
+            if (index < 0 || index >= size) throw new IndexOutOfRangeException();
+            storage[index] = value;
+        }
+    }
+
+    public void Add(T value)
+    {
+        if (size == storage.Length) AllocateMemory();
+        storage[size] = value;
+        size++;
+    }
+
+    public void AddAll(T[] array)
+    {
+        foreach (T item in array) Add(item);
+    }
+
+    public void Insert(int index, T value)
+    {
+        if (index < 0 || index >= size) throw new IndexOutOfRangeException();
+        if (size == storage.Length) AllocateMemory();
+        for (int i = size; i > index; i--)
+        {
+            storage[i] = storage[i - 1];
+        }
+        storage[index] = value;
+        size++;
+    }
+
+    public void InsertAll(int index, T[] array)
+    {
+        for (int i = size - 1; i >= 0; i--) Insert(index, array[i]);
+    }
+
+    private void AllocateMemory()
+    {
+        T[] newStorage = new T[(int)(storage.Length * 1.5) + 1];
+        Array.Copy(storage, newStorage, storage.Length);
+        storage = newStorage;
+    }
+
+    public bool RemoveAll(T[] array)
+    {
+        bool allDeleted = true;
+        foreach(T item in array) 
+            if(!Remove(item)) allDeleted = false;
+        return allDeleted;
+    }
+
+    public bool Remove(object obj)
+    {
+        int index = IndexOf(obj);
+        if (index == -1) return false;
+        RemoveAt(index);
+        return true;
+    }
+
+    public T RemoveAt(int index)
+    {
+        if (index < 0 || index >= size) throw new IndexOutOfRangeException();
+        T removeElem = storage[index];
+        for (int i = index; i < size - 1; i++)
+        {
+            storage[i] = storage[i + 1];
+        }
+        storage[--size] = default;
+        return removeElem;
+    }
+
+    public void RetainAll(T[] array)
+    {
+        for(int i = 0; i < size; i++)
+        {
+            bool toDelete = true;
+            foreach(T item in array)
+            {
+                if (storage[i].Equals(item))
+                {
+                    toDelete = false;
+                    break;
+                }
+            }
+            if (toDelete)
+            {
+                RemoveAt(i);
+                i--;
+            }
+        }
+    }
+
+    public T[] SubList(int fromIndex, int toIndex)
+    {
+        if (fromIndex < 0 || toIndex < 0 || fromIndex >= size
+            || toIndex >= size || fromIndex > toIndex) throw new ArgumentException();
+        T[] newArray = new T[toIndex - fromIndex];
+        Array.Copy(storage, toIndex, newArray, 0, newArray.Length);
+        return newArray;
+    }
+
+    public int IndexOf(object obj) 
+    { 
+        for (int i = 0; i < size; i++)
+            if(obj.Equals(storage[i])) return i;
+        return -1;
+    }
+
+    public int LastIndexOf(object obj)
+    {
+        for (int i = size-1; i >= 0; i--)
+            if (obj.Equals(storage[i])) return i;
+        return -1;
+    }
+
+    public bool Contains(object value) => IndexOf(value) != -1;
+
+    public bool ContainsAll(T[] array)
+    {
+        foreach(T item in array) 
+            if(!Contains(item)) return false;
+        return true;
+    }
+
+    public T[] ToArray()
+    {
+        T[] newStorage = new T[size];
+        Array.Copy(storage, newStorage, size);
+        return newStorage;
+    }
+
+    public void ToArray(T[] array, int itemCnt, int storageBegin = 0, int arrayBegin = 0)
+    {
+        Array.Copy(storage, storageBegin, array, arrayBegin, itemCnt);
+    }
+
+    public void Clear()
+    {
+        Array.Clear(storage, 0, size);
+        size = 0;
+    }
+
+    public override string ToString()
+        => $"[{string.Join(", ", ToArray())}]";
+}
---
+++ CASD_Task5/CASD_Task5/Program.cs
@@ -0,0 +1,86 @@
+﻿namespace CASD_Task5;
+using MyArrayList;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        string filePath = "index.txt";
+        Console.WriteLine(GetUniqueTags(filePath));
+    }
+
+    public static MyArrayList<string> GetUniqueTags(string filePath)
+    {
+        MyArrayList<string> tags = GetTags(filePath);
+        for (int i = 1; i < tags.Size(); i++) 
+        { 
+            for (int j = i - 1; j >= 0; j--)
+            {
+                if (isSameTags(tags[j], tags[i]))
+                {
+                    tags.RemoveAt(j);
+                    i--;
+                }
+            }
+        }
+        return tags;
+    }
+
+    public static bool isSameTags(string tag1, string tag2) { 
+        MyArrayList<char> symbols1 = new MyArrayList<char>(tag1.ToCharArray());
+        MyArrayList<char> symbols2 = new MyArrayList<char>(tag2.ToCharArray());
+        symbols1.Remove('/');
+        symbols2.Remove('/');
+        if (symbols1.Size() == symbols2.Size())
+        {
+            for (int i = 0; i < symbols1.Size(); i++)
+            {
+                if (!(symbols1[i] == symbols2[i] || Math.Abs(symbols1[i] - symbols2[i]) == 32))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+
+    public static MyArrayList<string> GetTags(string filePath)
+    {
+        MyArrayList<string> tags = new MyArrayList<string>();
+        using (var streamReader = new StreamReader(filePath))
+        {
+            string fileData = streamReader.ReadToEnd();
+            for(int i = 0; i < fileData.Length; i++)
+            {
+                string tag = "<";
+                if (fileData[i] == '<')
+                {
+                    i++;
+                    while (i < fileData.Length && fileData[i] != '>') {
+                        tag += fileData[i];
+                        i++;
+                    }
+                    if(i < fileData.Length) tag += fileData[i];
+                    if (isTag(tag)) tags.Add(tag);
+                }
+            }
+        }
+        return tags;
+    }
+
+    public static bool isTag(string tag)
+    {
+        int cntSlash = 0;
+        bool itsOk = true;
+        for(int i = 1; itsOk && i < tag.Length - 1; i++)
+        {
+            char c = tag[i];
+            if(c== '/') cntSlash++;
+            else if (!(c >= 48 && c <= 58 && i != 0 || c >= 65 && c <= 90 
+                || c >= 97 && c <= 122)) itsOk = false;
+        }
+        return tag[0] == '<' && tag[tag.Length - 1] == '>'
+            && (cntSlash == 1 && tag[1] == '/' || cntSlash == 0) && itsOk;
+    }
+}
