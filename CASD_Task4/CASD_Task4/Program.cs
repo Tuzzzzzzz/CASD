e0bb21b7fb8f11b358b15bcbca37c851dc0fe557
evgen <tuzee101005@gmail.com>
2024-09-22 03:36:40 +0300

    first commit for task4

---
+++ CASD_Task4/CASD_Task4/Program.cs
@@ -0,0 +1,12 @@
+﻿using MyArrayList;
+
+namespace CASD_Task4;
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        var arrayList = new MyArrayList<char>(['a', 'b', 'c', 'c', 'd']);
+        arrayList.RetainAll(['a', 'c']);
+        Console.WriteLine(arrayList);
+    }
+}
