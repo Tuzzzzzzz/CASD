271331e866f58c535b3e9bede9a59949baff7f99
evgen <tuzee101005@gmail.com>
2024-10-12 18:42:09 +0300

    first commit for task11

---
+++ CASD_Task11/CASD_Task11/Program.cs
@@ -0,0 +1,14 @@
+﻿using MyPriorityQueue;
+
+var pq = new MyPriorityQueue<int>(
+    [1, 5, 10, 8, 2, 2, 2, 11],
+    //сортировка по остаткам от пяти
+    (a, b) => a % 5 > b % 5 ? 1 : -1
+);
+
+while (!pq.Empty) {
+    var d = pq.Pool();
+    Console.WriteLine(
+        $"Значение: {d} Остаток: {d%5}"
+    );
+};
