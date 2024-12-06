1c7fa13ad5a8cf429d8a9a83a78d81e99ec847e5
evgen <tuzee101005@gmail.com>
2024-12-06 16:46:53 +0300

    first commit for task16

---
+++ CASD_Task16/CASD_Task16/MyListIterator.cs
@@ -0,0 +1,14 @@
+﻿public interface MyListIterator<T> : MyIterator<T>
+{
+    public bool HasPrevious();
+
+    public T Previous();
+
+    public int NextIndex();
+
+    public int PreviousIndex();
+
+    public void Set(T value);
+
+    public void Add(T value);
+}
---
+++ CASD_Task16/CASD_Task16/Program.cs
@@ -0,0 +1,9 @@
+﻿using MyLinkedList;
+
+var list = new MyLinkedList<int>([1,2,3,4]);
+foreach(var el in list.Where(p => p%2 == 0))
+{
+    Console.WriteLine(el);
+}
+
+
